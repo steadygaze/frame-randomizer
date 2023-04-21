22136419eeebf43f33a6c6449bbaf16ce30fc6e5
Steady Gaze <119086591+steadygaze@users.noreply.github.com>
2023-04-20 20:48:15 -0700

    feat: add helper to chunk match indices together

--- utils/fuzzy.test.ts
+++ utils/fuzzy.test.ts
@@ -1,5 +1,9 @@
 import { describe, expect, it } from "vitest";
-import { naiveFuzzySearch, naiveFuzzySearchParts } from "./fuzzy";
+import {
+  chunkMatchText,
+  naiveFuzzySearch,
+  naiveFuzzySearchParts,
+} from "./fuzzy";
 
 describe("fuzzy search", () => {
   it("should not match empty", () => {
@@ -104,5 +108,89 @@ describe("fuzzy search (parts output)", () => {
         { part: "w bridge", matching: false },
       ],
     });
+  });
+});
+
+describe("chunkMatchText (match text chunker)", () => {
+  it("should chunk a match at the start of the text", () => {
+    expect(
+      chunkMatchText({
+        indices: [[0, 2]],
+        value: "Old Man's War",
+        key: "title",
+      })
+    ).toEqual([
+      { part: "Old", matching: true },
+      { part: " Man's War", matching: false },
+    ]);
+  });
+
+  it("should chunk a match at the end of the text", () => {
+    expect(
+      chunkMatchText({
+        indices: [[10, 12]],
+        value: "Old Man's War",
+        key: "title",
+      })
+    ).toEqual([
+      { part: "Old Man's ", matching: false },
+      { part: "War", matching: true },
+    ]);
+  });
+
+  it("should chunk multiple matches in the middle of the text", () => {
+    expect(
+      chunkMatchText({
+        indices: [
+          [4, 4],
+          [13, 14],
+        ],
+        value: "The DaVinci Code",
+        key: "title",
+      })
+    ).toEqual([
+      { part: "The ", matching: false },
+      { part: "D", matching: true },
+      { part: "aVinci C", matching: false },
+      { part: "od", matching: true },
+      { part: "e", matching: false },
+    ]);
+  });
+
+  it("should chunk matches at both start and end of the text", () => {
+    expect(
+      chunkMatchText({
+        indices: [
+          [0, 4],
+          [14, 15],
+        ],
+        value: "The DaVinci Code",
+        key: "title",
+      })
+    ).toEqual([
+      { part: "The D", matching: true },
+      { part: "aVinci Co", matching: false },
+      { part: "de", matching: true },
+    ]);
+  });
+
+  it("should chunk matches in start, middle, and end of the text", () => {
+    expect(
+      chunkMatchText({
+        indices: [
+          [0, 4],
+          [6, 9],
+          [14, 15],
+        ],
+        value: "The DaVinci Code",
+        key: "title",
+      })
+    ).toEqual([
+      { part: "The D", matching: true },
+      { part: "a", matching: false },
+      { part: "Vinc", matching: true },
+      { part: "i Co", matching: false },
+      { part: "de", matching: true },
+    ]);
   });
 });
--- utils/fuzzy.ts
+++ utils/fuzzy.ts
@@ -1,4 +1,6 @@
-function naiveFuzzySearch(searchText: string, text: string) {
+import Fuse from "fuse.js";
+
+export function naiveFuzzySearch(searchText: string, text: string) {
   let searchTextIndex = 0;
   let matchStartIndex = 0;
   let matchState = false;
@@ -31,7 +33,7 @@ export interface FuzzySearchResult {
   parts: SearchPart[];
 }
 
-function naiveFuzzySearchParts(
+export function naiveFuzzySearchParts(
   searchText: string,
   text: string
 ): FuzzySearchResult {
@@ -109,4 +111,50 @@ function naiveFuzzySearchParts(
   };
 }
 
-export { naiveFuzzySearch, naiveFuzzySearchParts };
+export interface FuseMatch {
+  indices: number[][];
+  value: string;
+  key: string;
+}
+
+export function chunkMatchText(match: Fuse.FuseResultMatch): SearchPart[] {
+  if (match.indices.length <= 0) {
+    throw new Error("No matches");
+  }
+
+  const parts = [];
+  for (let index = 0; index < match.indices.length; ++index) {
+    const [start, end] = match.indices[index];
+    if (index === 0) {
+      if (start !== 0) {
+        // First matched range starts after zero, meaning there is an unmatched
+        // range at the beginning of the text.
+        parts.push({
+          part: match.value?.substring(0, start) || "",
+          matching: false,
+        });
+      } // Else, matched range starts from beginning.
+    } else {
+      const [, prevEnd] = match.indices[index - 1];
+      parts.push({
+        part: match.value?.substring(prevEnd + 1, start) || "",
+        matching: false,
+      });
+    }
+    parts.push({
+      part: match.value?.substring(start, end + 1) || "",
+      matching: true,
+    });
+  }
+  const [, lastEnd] = match.indices[match.indices.length - 1];
+  if (lastEnd + 1 !== match.value?.length) {
+    // Last matched range doesn't end at the end, meaning there is an unmatched
+    // range at the end of the text.
+    parts.push({
+      part: match.value?.substring(lastEnd + 1, match.value?.length) || "",
+      matching: false,
+    });
+  }
+
+  return parts;
+}
