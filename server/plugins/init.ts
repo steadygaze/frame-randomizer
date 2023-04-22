c70abc89a0062ce05e1dd2d5490bd6e2c35b2078
Steady Gaze <119086591+steadygaze@users.noreply.github.com>
2023-04-21 23:40:27 -0700

    feat: clear storage on restart
    Assuming /tmp is a tmpfs, we can simply forget all previous images.

--- server/plugins/init.ts
+++ server/plugins/init.ts
@@ -1,7 +1,12 @@
 import { RuntimeConfig } from "nuxt/schema";
 import { getEpisodeData } from "../load";
 
+async function clearImageStorage() {
+  await useStorage("genimg").clear();
+}
+
 export default defineNitroPlugin(() => {
   // Side effect: result will be cached and ready.
   getEpisodeData(useRuntimeConfig() as RuntimeConfig);
+  clearImageStorage();
 });
