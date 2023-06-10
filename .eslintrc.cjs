d290a7f956073b6f7781f98140c2773c5a00ad2f
Steady Gaze <119086591+steadygaze@users.noreply.github.com>
2023-06-09 21:54:29 -0400

    refactor: move to multi-page app
    Previously this was a single-page app with no pages/ directory where
    every page is just app.vue. Change to a regular multi-page app in
    preparation for i18n redirects (where different languages live at
    different paths).

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,5 +9,6 @@ module.exports = {
   rules: {
     "no-console": ["error", { allow: ["warn", "error"] }],
     "prettier/prettier": ["error"],
+    "vue/multi-word-component-names": "off",
   },
 };
---
+++ pages/index.vue
@@ -0,0 +1,84 @@
+<template>
+  <div id="main">
+    <div id="inputpane">
+      <FuzzyInput />
+    </div>
+    <div id="imagepane">
+      <RandomImage />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useHead, useRuntimeConfig } from "#app";
+
+const config = useRuntimeConfig();
+useHead({
+  title: config.public.instanceName,
+  meta: [{ name: "description", content: config.public.metaDescription }],
+});
+</script>
+
+<style>
+html,
+body {
+  height: 100%;
+  width: 100%;
+  margin: 0;
+  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
+  background-color: black;
+  overflow: hidden; /* No page-level scrollbars. */
+}
+
+body > * {
+  /* The parent div that Nuxt adds. */
+  height: 100%;
+  width: 100%;
+}
+
+#main {
+  background-color: white;
+  display: flex;
+  flex-direction: row;
+  height: 100%;
+  width: 100%;
+  box-sizing: border-box;
+  align-items: stretch;
+  justify-content: stretch;
+}
+
+#inputpane {
+  overflow-y: scroll; /* Otherwise scrollbar appears/disappears as searches happen, which is distracting. */
+  background-color: #eee;
+  width: 100%;
+  max-width: 500px;
+  min-width: 200px;
+  height: 100%;
+  flex: 1 3 20%;
+  padding: 0;
+  margin: 0;
+}
+
+#imagepane {
+  flex: 10 0 60%;
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+  background-color: black;
+}
+
+@media screen and (max-aspect-ratio: 12/10) {
+  #main {
+    flex-direction: column;
+  }
+  #inputpane {
+    max-width: 100%;
+    flex: 5 0 40%;
+    order: 2;
+  }
+  #imagepane {
+    flex: 1 0 0%;
+    order: 1;
+  }
+}
+</style>
