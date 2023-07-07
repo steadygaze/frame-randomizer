steadygaze/frame-randomizer

commit 61427325f2f9e75f17da5894e359d9b28f9a775f

fix: stop deleting answer/image when tabbing away
The visibilitychange event triggers when tabbing away, and is
potentially the last event observed before the user closes the app, and
is normally good for saving state before the user exits.  However, it
also happens when tabbing away or switching apps, which makes it too
eager to clean up the answer and image.

components/RandomImage.vue
@@ -68,20 +68,6 @@ function handleError() {
   imageIsLoading.value = false;
   showImageError.value = true;
 }
-
-onMounted(() => {
-  if (!document) return;
-  document.addEventListener("visibilitychange", function () {
-    if (
-      document.visibilityState === "hidden" &&
-      (detectBrowser()?.name !== "firefox" || waitingForGuess.value) &&
-      !cleanedUpFrame.value
-    ) {
-      navigator.sendBeacon(`/api/frame/cleanup/${frameId.value}`);
-      cleanedUpFrame.value = true;
-    }
-  });
-});
 </script>
 
 <style scoped>

commit 319a762e19fa7ae15bfd36035f5c60f5d29c6b62

style: fix lint errors
Introduced by 61427325f2f9e75f17da5894e359d9b28f9a775f.

components/RandomImage.vue
@@ -25,7 +25,7 @@ const readySvg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 320">
 
 const extension = useRuntimeConfig().public.imageOutputExtension;
 const appStateStore = useAppStateStore();
-const { detectBrowser, readout } = appStateStore;
+const { readout } = appStateStore;
 const {
   browser,
   cleanedUpFrame,
@@ -34,7 +34,6 @@ const {
   imageLoadError,
   imageLoadTimestamp,
   runReadyState,
-  waitingForGuess,
 } = storeToRefs(appStateStore);
 
 const showImageError = ref(false);