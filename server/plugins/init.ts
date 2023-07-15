13919eb15c9ce483086a72352f9281518ac96e6b
Steady Gaze <119086591+steadygaze@users.noreply.github.com>
2023-07-15 16:21:50 -0700

    refactor: remove obsolete storage init code

--- server/plugins/init.ts
+++ server/plugins/init.ts
@@ -1,14 +1,6 @@
 import { getFrameProducerQueue } from "../load";
 
-/**
- *
- */
-async function clearImageStorage() {
-  await useStorage("genimg").clear();
-}
-
 export default defineNitroPlugin(() => {
   // Side effect: result will be cached and ready.
   getFrameProducerQueue(useRuntimeConfig());
-  clearImageStorage();
 });
